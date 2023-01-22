0d90460ff408981b2e545370b9baba3eb42769ee
Joshvico <97739027+Joshvico@users.noreply.github.com>
2023-01-22 16:01:07 +0100

    first commit

---
+++ input.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+
+var fname = prompt("Enter Your first name : ");
+var lname = prompt("Enter Your last name : ");
+
+console.log("Your full Name is", fname, lname);
