ec40a031b597e69dadbdef9991ec9ad7daaae64f
Joshvico <97739027+Joshvico@users.noreply.github.com>
2023-07-15 13:08:34 +0200

    more examples on switch statements

--- JAVA SCRIPT/Boolean JS/Boolean console.js
+++ JAVA SCRIPT/Boolean JS/Boolean console.js
@@ -250,7 +250,33 @@ break;
 
 switchOfStuff(1);
 console.log(switchOfStuff(""));
+
+/* MULTIPLE IDENTICAL OPTIONS WITH SWICTH */
+function sequentialSizes(val) {
+  let answer = "";
+switch(val){
+  case 1:
+  case 2:
+  case 3:
+    answer = "Low";
+    break;
+  case 4:
+  case 5:
+  case 6:
+    answer = "Mid";
+    break;
+  case 7:
+  case 8:
+  case 9:
+    answer = "High";
+    break;
   
+}
+  return answer;
+}
+
+sequentialSizes(1);
+console.log (sequentialSizes(6));
 
   
     
