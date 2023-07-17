34bc50e09a690c4566a3215b8ce53c73722dfc4f
Joshvico <97739027+Joshvico@users.noreply.github.com>
2023-07-17 21:03:42 +0200

    couting cards and practicing if else statement

--- JAVA SCRIPT/Printing test.js
+++ JAVA SCRIPT/Printing test.js
@@ -1,1 +1,29 @@
-console.log("keeee"); 
+
+
+function insult(name, more){
+  
+  console.log ("tu puta madre " + name + more);
+}
+
+insult("Juan", " tio!");
+insult ("Maria");
+
+
+function dayMessahe(hour) {
+
+  if (hour >= 6 &&  hour <= 12) {
+    return "good Morning";
+  } 
+  
+  else if (hour > 12 && hour <= 20) {
+    return "Good After Noon";
+  } else if (hour > 20 && hour < 24){
+    return "Good Night";
+  } else if (hour >= 1 && hour < 6){
+    return "You should be Sleeping";
+  } else {
+    return "Enter Hour";
+  }
+}
+
+console.log(dayMessahe());
--- JAVA SCRIPT/tempCodeRunnerFile.js
+++ JAVA SCRIPT/tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-    // Only change code below this line
+Juan
