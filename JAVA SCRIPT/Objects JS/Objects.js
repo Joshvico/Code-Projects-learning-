9b0d358ee36e14c70e0d690d05300c7b36942a66
Joshvico <97739027+Joshvico@users.noreply.github.com>
2023-07-18 23:35:10 +0200

    pending accesing object []

---
+++ JAVA SCRIPT/Objects JS/Objects.js
@@ -0,0 +1,23 @@
+/* CREATING AND OBJECT */
+const myDog = {
+  "name":"Leo",
+  legs: 4, /* watch legs, it should be a string--> if " are not set for strings JS will read them as strings" */
+  "tails": 1,
+  friends: ["mama", "Manel"] /* if " are not set for strings JS will read them as strings" */
+}; /* my the ; to finnish the objects it is like declaring a variable or a statement */
+
+// ACCESING OBJECTS  PROPERTIES with . 
+
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+const hatValue = testObj.hat; /* . is used when knowing the name of the property even if it is a string you can call it like if not */
+const shirtValue = testObj.shirt; 
+
+
+// ACCESING OBJECTS  PROPERTIES with [] 
+
+
