e6e5e03aa6d2ad106495d1f7f69e63b95217d66a
Joshvico <97739027+Joshvico@users.noreply.github.com>
2023-08-10 22:24:30 +0200

    Objects

---
+++ JAVA SCRIPT/Loops.js/Loops Console.js
@@ -0,0 +1,10 @@
+
+/* WHILE LOOP */
+const myArray = [];
+
+var i = 5;
+while(i >= 0) { /* mind >= to include 0 */
+  myArray.push(i); /* every action is delimited with ; */
+  i--;/* this action delimitates the count */
+}
+console.log(myArray); /* this will loop code until reachin 0 [5,4,3,2,1,00] */
