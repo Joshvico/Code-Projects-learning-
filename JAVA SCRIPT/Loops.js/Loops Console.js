252d35b71962d91a64eb2080163740c7c0883421
Joshvico <97739027+Joshvico@users.noreply.github.com>
2023-08-10 22:27:51 +0200

    While Loop

--- JAVA SCRIPT/Loops.js/Loops Console.js
+++ JAVA SCRIPT/Loops.js/Loops Console.js
@@ -8,3 +8,5 @@ while(i >= 0) { /* mind >= to include 0 */
   i--;/* this action delimitates the count */
 }
 console.log(myArray); /* this will loop code until reachin 0 [5,4,3,2,1,00] */
+
+  /* FOOR LOOP */
--- JAVA SCRIPT/Loops.js/Loops.js
+++ JAVA SCRIPT/Loops.js/Loops.js
@@ -1,1 +1,12 @@
 /* WHILE LOOP */
+
+const myArray = [];
+
+var i = 5;
+while(i >= 0) { /* mind >= to include 0 */
+  myArray.push(i); /* every action is delimited with ; */
+  i--;/* this action delimitates the count */
+}
+console.log(myArray); /* this will loop code until reachin 0 [5,4,3,2,1,00] */
+
+/* FOOR LOOP */
