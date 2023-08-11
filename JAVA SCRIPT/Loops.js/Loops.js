4a31cc898eeb96010e684ffbfb2eb88520cb2468
Joshvico <97739027+Joshvico@users.noreply.github.com>
2023-08-11 19:14:03 +0200

    trying other while loops

--- JAVA SCRIPT/Loops.js/Loops.js
+++ JAVA SCRIPT/Loops.js/Loops.js
@@ -2,11 +2,20 @@
 
 const myArray = [];
 
-var i = 5;
+var i = 10;
 while(i >= 0) { /* mind >= to include 0 */
   myArray.push(i); /* every action is delimited with ; */
   i--;/* this action delimitates the count */
 }
 console.log(myArray); /* this will loop code until reachin 0 [5,4,3,2,1,00] */
+
+const myArray1 = [1,2,3,4,5,6,7,8,9,10];
+
+var i = 10;
+while(i >= 0) { /* mind >= to include 0 */
+  myArray.unshift(i); /* every action is delimited with ; */
+  i++;/* this action delimitates the count */
+}
+console.log(i);
 
 /* FOR LOOP */
