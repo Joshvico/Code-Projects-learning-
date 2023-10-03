1b3da5557922dedfe3730f086223c532703349b4
Joshvico <97739027+Joshvico@users.noreply.github.com>
2023-10-03 10:02:21 +0200

    js CHALLENGES

--- JAVA SCRIPT/Objects JS/Objects Console.js
+++ JAVA SCRIPT/Objects JS/Objects Console.js
@@ -242,3 +242,47 @@ function updateRecords(records, id, prop, value) {
   return records;
 }
 console.log(recordCollection[1245].artist);
+
+
+  /*PROFILE LOOKUP excercises*/
+  const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+  
+  function lookUpProfile(name, prop) {
+  for (var i= 0; i < contacts.length; i++) { /* here the loop acces the whole object thanks to lenght. */
+    if (contacts[i]["firstName"] === name) {
+      if (contacts[i].hasOwnProperty(prop)) {
+        return contacts[i][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact"; /* here we are exiting the loop + boolen condition and return "No such a contact" */
+  }
+  
+  console.log (lookUpProfile("Akira", "address"));
