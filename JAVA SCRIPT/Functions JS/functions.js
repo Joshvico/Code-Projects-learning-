ce794caf0d6213b0c4f00ef493e0c06f16fc1dba
Joshvico <97739027+Joshvico@users.noreply.github.com>
2023-10-22 22:39:24 +0200

    starting nodule ES6

--- JAVA SCRIPT/Functions JS/functions.js
+++ JAVA SCRIPT/Functions JS/functions.js
@@ -164,3 +164,24 @@ function countdown(n){
   }  
   
   console.log (rangeOfNumbers(1,5));
+/* 
+  ********************************
+  ********************************
+  ********************************
+  ********************************
+  ES6 */
+
+  /* Var VS let Scope EX from Exercise ES6 Freecode Camp Module */
+
+
+  function checkScope() {
+    let i = 'function scope'; /* i is declared here with this value as is let is in this function {}scope the we can declare i again with another value as the scoend let i is on inside expresion scope */
+    if (true) {
+      let i = 'block scope'; /* The scope of i value is inside this boolean */
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i; /* the firs let i will be printed in the end as the boolen statement is what runs first and the retunr from function prints */
+  }
+
+  console.log ( checkScope()); 
