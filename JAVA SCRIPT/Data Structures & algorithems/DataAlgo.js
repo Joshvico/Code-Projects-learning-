5196a3d45a89703294f5f66acde8284d63e297f8
Joshvico <97739027+Joshvico@users.noreply.github.com>
2023-11-07 19:56:01 +0100

    practicing with Arrays

--- JAVA SCRIPT/Data Structures & algorithems/DataAlgo.js
+++ JAVA SCRIPT/Data Structures & algorithems/DataAlgo.js
@@ -1,1 +1,24 @@
-  var yourArray = [1, "Hello, World!", true, 42, false];
+/* MANIPULATING ARRAYS WITH UNSHIFT. and PUSH. */
+
+let twentyThree = 'XXIII';
+let romanNumerals = ['XXI', 'XXII'];
+
+romanNumerals.unshift('XIX', 'XX'); /* puts in front [ *XIX*, *XX*, 'XXI', 'XXII' ] */
+
+romanNumerals.push(twentyThree); /* puts behind [ 'XIX', 'XX', 'XXI', 'XXII', *XXIII* ] */
+
+console.log(romanNumerals);
+
+/* PRACTICING MORE ARRAY MANIPULAITON WITH PUSH AND UNSHIFT */
+
+let mixingnumbers = [];
+
+function mixedNumbers(arr) {
+  arr.unshift("I", 2, "three"); // Corrected syntax for unshift
+  arr.push(7, 'VIII', 9); // Corrected syntax for push
+  mixingnumbers.push(...arr); // Corrected syntax for pushing the elements of 'arr' into 'mixingnumbers'
+}
+
+mixedNumbers(['IV', 5, 'six']);
+console.log(mixingnumbers);
+
---
+++ JAVA SCRIPT/Data Structures & algorithems/DataAlgoConsole.js
@@ -0,0 +1,13 @@
+/* MANIPULATING ARRAYS WITH SHIFT. and PUSH. */
+
+let twentyThree = 'XXIII';
+let romanNumerals = ['XXI', 'XXII'];
+
+romanNumerals.unshift('XIX', 'XX'); /* puts in front [ *XIX*, *XX*, 'XXI', 'XXII' ] */
+
+romanNumerals.push(twentyThree); /* puts behind [ 'XIX', 'XX', 'XXI', 'XXII', *XXIII* ] */
+
+console.log(romanNumerals);
+
+
+
