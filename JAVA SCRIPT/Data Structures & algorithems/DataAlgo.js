76d2c7295485d77077bab1bd8f644e6ecd699ac9
Joshvico <97739027+Joshvico@users.noreply.github.com>
2023-11-08 17:42:32 +0100

    splice and slice

--- JAVA SCRIPT/Data Structures & algorithems/DataAlgo.js
+++ JAVA SCRIPT/Data Structures & algorithems/DataAlgo.js
@@ -45,6 +45,20 @@ arr.splice(0,1)
 arr.splice(3,4)
 // Only change code above this line
 console.log(arr); /* The goal is to remove to make an array that sums 10 */
- 
 
-*/
+/*ADDING NUMS USING SPLICE (x,x,x) 3 arguments position, elements to splice and new agurment to add */
+
+/*splice (position, amount to delete form position, new arguments to add…) */
+
+function htmlColorNames(arr) {
+  arr.splice(0,2,"DarkSalmon","BlanchedAlmond","e","g","r","5")    
+ return arr;
+ }
+ console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick'])); /* it will console.lof this [ 'DarkSalmon','BlanchedAlmond','LavenderBlush','PaleTurquoise','FireBrick' ] */
+ 
+/* EXTRACTING ELEMENTS FROM ARRAY WITH SLICE (X,X) */
+
+function forecast(arr) {
+  return arr.slice(2, 4); 
+}
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
--- JAVA SCRIPT/Data Structures & algorithems/DataAlgoConsole.js
+++ JAVA SCRIPT/Data Structures & algorithems/DataAlgoConsole.js
@@ -40,7 +40,20 @@ arr.splice(3,4)
 // Only change code above this line
 console.log(arr); /* The goal is to remove to make an array that sums 10 */
 
-/*ADDING NUMS USING SPLICE */
+/*ADDING NUMS USING SPLICE (x,x,x) 3 arguments position, elements to splice and new agurment to add */
 
+/*splice (position, amount to delete form position, new arguments to add…) */
 
+function htmlColorNames(arr) {
+ arr.splice(0,2,"DarkSalmon","BlanchedAlmond") /* This removes the first 2 elements and add from starting/deleting position the 2 new parameters added or any amount you want!! */   
+return arr;
+}
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick'])); /* it will console.lof this [ 'DarkSalmon','BlanchedAlmond','LavenderBlush','PaleTurquoise','FireBrick' ] */
+
+/* EXTRACTING ELEMENTS FROM ARRAY WITH SLICE (X,X) */
+
+function forecast(arr) { /* slice only take 2 paramenters that act like a selector range */
+  return arr.slice(2, 4); /*  we are extracting elements from enetered arrays from 2nd posiiton to 4th // this only includes intialia element and element in betweenand the */
+}
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms'])); /*  it will return "warm and sunny" nooooooot "cool" */
 
