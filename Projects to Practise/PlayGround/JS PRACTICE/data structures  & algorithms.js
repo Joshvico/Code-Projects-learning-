27869a92a947ee3e87cd396adc3ccc1271379c05
Joshvico <97739027+Joshvico@users.noreply.github.com>
2023-11-13 22:45:49 +0100

    practicing some data structure and algo excer.

---
+++ Projects to Practise/PlayGround/JS PRACTICE/data structures  & algorithms.js
@@ -0,0 +1,93 @@
+/* Exercise 1: Iterate and Modify Nested Objects */
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42,
+    'offline power': 67,
+    'test Array': ["melocton", "pecador", 4]
+  }
+};
+
+const updateuserActivity = (newdata,newArray) => {
+userActivity.data.online = newdata; /*  no need for "()" */
+userActivity.data["test Array"][2]= newArray;
+return userActivity;
+  
+}
+
+console.log (updateuserActivity(74,"hello there"));
+
+
+/* Exercise 2: Check and Add Properties */
+
+let inventory = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  'strawberries forever': 27
+};
+
+const checkInventory = (key) => {
+  if (inventory.hasOwnProperty(key)) {
+    return `${key} are available. Current units: ${inventory[key]}. How many ${key} would you like to get?`;
+  }
+  return `The entered item "${key}" does not exist or is not available.`;
+}
+
+console.log(checkInventory("bananas"));
+
+const addInventory = (key, quant) => {
+  if (inventory.hasOwnProperty(key)) {
+    if (quant <= inventory[key]) {
+      return `${quant} ${key}? Yes, we have enough. There are ${inventory[key]} available. How many ${key} would you like to get?`;
+    } else {
+      return `Sorry, we currently have ${inventory[key]}. We don't have ${quant} you're asking for.`;
+    }
+  } else {
+    return `The entered item "${key}" does not exist or is not available.`;
+  }
+}
+
+console.log(addInventory("bananas", 3));
+
+/* Exercise 3: Delete and Check Properties */
+
+let foodPreferences = {
+  vegetarian: true,
+  glutenFree: false,
+  nutFree: true,
+  dairyFree: false
+};
+
+const newObject = (keys) => {
+  delete foodPreferences[keys];
+  return foodPreferences;
+}
+ 
+
+ console.log(newObject("nutFree"));
+
+/*  const checkParameter = (param) => {
+  if (foodPreferences.hasOwnProperty(param)) {
+    return true;
+  }
+  return false;
+ }
+
+console.log (checkParameter("puto"));
+ */
+/* 
+const checkParameter = (param) => {
+  return foodPreferences.hasOwnProperty(param);
+} */
+
+const checkParameter = (param) => foodPreferences.hasOwnProperty(param); /* this is the shortes version of this function */
+
+console.log (checkParameter("dairyFree"));
+
+
+
--- Projects to Practise/PlayGround/playGround.js
+++
@@ -1,9 +0,0 @@
-const LOCAL_FORECAST = {
-  yesterday: { low: 61, high: 75 },
-  today: { low: 64, high: 77 },
-  tomorrow: { low: 68, high: 80 }
-  };
-  
-const { today: {low: lowToday1, high: highToday3}}  = LOCAL_FORECAST;
-
-console.log ();
