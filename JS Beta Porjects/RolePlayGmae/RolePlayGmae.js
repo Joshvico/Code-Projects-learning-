b8757f783bacbfc3cf0d7970a4a30c61e73b56ac
Joshvico <97739027+Joshvico@users.noreply.github.com>
2024-01-26 07:32:52 +0100

    more changes in the games terniary

--- JS Beta Porjects/RolePlayGmae/RolePlayGmae.js
+++ JS Beta Porjects/RolePlayGmae/RolePlayGmae.js
@@ -203,3 +203,18 @@ function defeatMonster() {
   xpText.innerText = xp;
   
 }
+
+function attack() {
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+    lose();
+  } 
+  
+  monsterHealth <= 0? winGame(fighting === 2) : defeatMonster();
+  
+}
