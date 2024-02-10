57498f8a474290271b20cad9ad6ad8e0714b5171
Joshvico <97739027+Joshvico@users.noreply.github.com>
2024-02-10 17:25:03 +0100

    Review concepts for the weekends Array Methods

---
+++ Weekend concepts Review/ARRAY METHODS.js
@@ -0,0 +1,12 @@
+/* ARROW FUNCTIONS */
+
+function name (paramente) {
+return "something" + Parameter 
+};
+
+/* arrow when there a  */
+const func = (paramater) => {
+  return "something" + Parameter 
+
+}
+
