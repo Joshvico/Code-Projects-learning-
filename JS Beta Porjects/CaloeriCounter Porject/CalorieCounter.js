fb6da3263bb62cb8feb55a77bcd638cac1cb2956
Joshvico <97739027+Joshvico@users.noreply.github.com>
2024-02-11 18:35:10 +0100

    adding a for of loop + Node list.

--- JS Beta Porjects/CaloeriCounter Porject/CalorieCounter.js
+++ JS Beta Porjects/CaloeriCounter Porject/CalorieCounter.js
@@ -33,7 +33,16 @@ function addEntry() {
 }
 
 function getCaloriesFromInputs(list) {
+  let calories = 0;
 
+  for (const item of list) {
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+
+    if (invalidInputMatch) {
+
+    }
+  }
 }
 
 addEntryButton.addEventListener("click", addEntry);
