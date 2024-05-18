09d1aa6677ee86df2aab41cb1be3bdb95b39efc8
Joshvico <97739027+Joshvico@users.noreply.github.com>
2024-05-18 12:46:48 +0200

    testing nice excercise

---
+++ Bootstrap/tetsing resutl.js
@@ -0,0 +1,9 @@
+function JavaScript () {
+
+  var result = 2 +"2"-2;/* When in Java after + ther is "string" concatenation happens so it his considering 22 not 2+2 ¡ */
+
+  console.log (result); 
+
+}
+
+JavaScript ();
